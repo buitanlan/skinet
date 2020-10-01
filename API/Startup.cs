131f4a4a27b7ccc9a33b56ebd10809432a434e77
BÙI TẤN LÂN <tanlanhcmus@gmail.com>
2020-10-01 23:50:17 +0700

    add and resolver picture

--- API/Startup.cs
+++ API/Startup.cs
@@ -60,7 +60,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseStaticFiles();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
