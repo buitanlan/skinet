1c740cce65074c53eb9077ce4ac008d2c74b8a21
BÙI TẤN LÂN <tanlanhcmus@gmail.com>
2020-10-06 09:34:32 +0700

    fe/add filter function

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
 import { IProduct } from '../shared/models/product';
+import { IType } from '../shared/models/type';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -10,14 +12,48 @@ import { ShopService } from './shop.service';
 })
 export class ShopComponent implements OnInit {
   products: IProduct[];
+  brands: IBrand[];
+  types: IType[];
+  brandIdSelected = 0;
+  typeIdSelected = 0;
   constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
-    this.shopService.GetProduct().subscribe((response: IPagination) => {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+  // tslint:disable-next-line:typedef
+  getProducts(){
+    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected).subscribe((response: IPagination) => {
       this.products = response.data;
     }, error => {
       console.log(error);
     });
   }
-
+  // tslint:disable-next-line:typedef
+  getBrands() {
+    this.shopService.getBrand().subscribe((response: IBrand[]) => {
+      this.brands = [{ id: 0, name: 'All' }, ... response];
+    }, error => {
+      console.log(error);
+    });
+  }
+  // tslint:disable-next-line:typedef
+  getTypes() {
+    this.shopService.getType().subscribe((response: IType[]) => {
+      this.types = [{ id: 0, name: 'All' }, ...response];
+    }, error => {
+      console.log(error);
+    });
+  }
+  // tslint:disable-next-line:typedef
+  onBrandSelected(brandId: number) {
+    this.brandIdSelected = brandId;
+    this.getProducts();
+  }
+  onTypeSelected(typeId: number) {
+    this.typeIdSelected = typeId;
+    this.getProducts();
+  }
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,6 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
+import { IType } from '../shared/models/type';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +12,27 @@ export class ShopService {
   baseUrl = 'https://localhost:5001/api/';
   constructor(private http: HttpClient) { }
   // tslint:disable-next-line:typedef
-  GetProduct() {
-    return this.http.get<IPagination>(this.baseUrl + 'products');
+  getBrand()  {
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+  }
+  // tslint:disable-next-line:typedef
+  getType() {
+    return this.http.get<IType[]>(this.baseUrl + 'products/types');
+  }
+  // tslint:disable-next-line:typedef
+  getProducts(brandId?: number, typeId?: number) {
+    let params = new HttpParams();
+    if (brandId) {
+      params = params.append('brandId', brandId.toString());
+    }
+    if (typeId) {
+      params = params.append('typeId', typeId.toString());
+    }
+    return this.http.get<IPagination>(this.baseUrl + 'products', { observe: 'response', params })
+      .pipe(
+        map((response) => {
+          return response.body;
+        })
+      );
   }
 }
