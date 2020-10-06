443495c954344e2bed0e5cff2f6d459ce72a2959
BÙI TẤN LÂN <tanlanhcmus@gmail.com>
2020-10-06 19:35:11 +0700

    fe/add nav-bar link

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsDetailsComponent } from './shop/products-details/products-details.component';
+import { ShopComponent } from './shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'shop/:id', component: ProductsDetailsComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -3,15 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
+import { ProductsDetailsComponent } from './products-details/products-details.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [ShopComponent, ProductItemComponent],
-  imports: [
-    CommonModule,
-    SharedModule,
-  ],
-  exports: [ShopComponent]
+  declarations: [ShopComponent, ProductItemComponent, ProductsDetailsComponent],
+  imports: [CommonModule, SharedModule, RouterModule],
+  exports: [ShopComponent, ProductsDetailsComponent],
 })
-export class ShopModule { }
+export class ShopModule {}
