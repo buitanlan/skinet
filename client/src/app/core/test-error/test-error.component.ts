9cd96c9d5e2009fc53b8c81e3334a65a885affcc
BÙI TẤN LÂN <tanlanhcmus@gmail.com>
2020-10-07 12:04:29 +0700

    fe/handle validation error

--- client/src/app/core/test-error/test-error.component.ts
+++ client/src/app/core/test-error/test-error.component.ts
@@ -9,6 +9,7 @@ import { environment } from 'src/environments/environment';
 })
 export class TestErrorComponent implements OnInit {
   baseUrl = environment.apiUrl;
+  validationErrors: any;
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {}
@@ -34,7 +35,10 @@ export class TestErrorComponent implements OnInit {
   get400ValidationError() {
     this.http.get(this.baseUrl + 'products/fortytwo').subscribe(
       (response) => console.log(response),
-      (error) => console.log(error)
+      (error) => {
+        console.log(error);
+        this.validationErrors = error.errors;
+      }
     );
   }
 }
