5453ad55ae6b8a4c2e759868814fe14b0c35df71
BÙI TẤN LÂN <tanlanhcmus@gmail.com>
2020-10-07 10:15:11 +0700

    fe/create test ui

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+import { TestErrorComponent } from './test-error/test-error.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [NavBarComponent, TestErrorComponent, ServerErrorComponent, NotFoundComponent],
   imports: [
     CommonModule,
     RouterModule
