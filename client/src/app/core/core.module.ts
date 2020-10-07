79cf130e5b7eaca63e95cc37284bdbfe8cd3edef
BÙI TẤN LÂN <tanlanhcmus@gmail.com>
2020-10-07 11:44:40 +0700

    fe/add toast nofitication

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -5,6 +5,8 @@ import { RouterModule } from '@angular/router';
 import { TestErrorComponent } from './test-error/test-error.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 
@@ -12,7 +14,12 @@ import { NotFoundComponent } from './not-found/not-found.component';
   declarations: [NavBarComponent, TestErrorComponent, ServerErrorComponent, NotFoundComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true
+    })
   ],
   exports: [NavBarComponent]
 })
