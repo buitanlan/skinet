c596b8c6dafafa63d798e8d88eb0968ec9bd8e89
BÙI TẤN LÂN <tanlanhcmus@gmail.com>
2020-10-15 00:13:31 +0700

    fe/product detail to cart

--- client/src/app/shop/products-details/products-details.component.ts
+++ client/src/app/shop/products-details/products-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BasketService } from 'src/app/basket/basket.service';
 import { IProduct } from 'src/app/shared/models/product';
 import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
@@ -11,11 +12,26 @@ import { ShopService } from '../shop.service';
 })
 export class ProductsDetailsComponent implements OnInit {
   product: IProduct;
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute, private bcService: BreadcrumbService) {
+  quantity = 1;
+  constructor(private shopService: ShopService,
+              private activatedRoute: ActivatedRoute,
+              private bcService: BreadcrumbService,
+              private basketService: BasketService) {
     this.bcService.set('@productDetails', '');
    }
   ngOnInit(): void {
     this.loadProduct();
+  }
+  addItemToBasket() {
+    this.basketService.addItemToBasket(this.product, this.quantity);
+  }
+  incrementQuantity() {
+    this.quantity++;
+  }
+  decrementQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
   }
   loadProduct() {
     this.shopService.getProduct(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe(product => {
