0ca6dcb6b34f46b05fc64119f04b000d258bf5e1
BÙI TẤN LÂN <tanlanhcmus@gmail.com>
2021-02-03 17:51:19 +0700

    show and save address

--- client/src/app/checkout/checkout-address/checkout-address.component.ts
+++ client/src/app/checkout/checkout-address/checkout-address.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from 'src/app/account/account.service';
 
 @Component({
   selector: 'app-checkout-address',
@@ -9,9 +11,21 @@ import { FormGroup } from '@angular/forms';
 export class CheckoutAddressComponent implements OnInit {
   @Input() checkoutForm: FormGroup;
 
-  constructor() { }
+  constructor(private accountService: AccountService, private toastr: ToastrService) { 
+
+  }
 
   ngOnInit(): void {
+  }
+  saveUserAddress(){
+    this.accountService.updateUserAddress(this.checkoutForm.get('addressForm').value)
+    .subscribe(()=>{
+      this.toastr.success('Address saved');
+
+    }, err =>{
+      this.toastr.error(err.message);
+      console.log(err)
+    })
   }
 
 }
--- client/src/app/checkout/checkout.component.ts
+++ client/src/app/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AccountService } from '../account/account.service';
 
 @Component({
   selector: 'app-checkout',
@@ -9,10 +10,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class CheckoutComponent implements OnInit {
   checkoutForm: FormGroup;
 
-  constructor(private fb: FormBuilder ) {}
+  constructor(private fb: FormBuilder, private accountService: AccountService) {}
 
   ngOnInit(): void {
     this.createCheckoutForm();
+    this.getAddressFromValues();
   }
 
   createCheckoutForm(){
@@ -23,7 +25,7 @@ export class CheckoutComponent implements OnInit {
         street : [null, Validators.required],
         city : [null, Validators.required],
         state : [null, Validators.required],
-        zipcode : [null, Validators.required]
+        zipCode : [null, Validators.required]
       }),
       deliveryForm: this.fb.group({
         deliveryMethod: [null, Validators.required]
@@ -32,6 +34,16 @@ export class CheckoutComponent implements OnInit {
         nameOnCard: [null, Validators.required]
       })
     });
+  }
+
+  getAddressFromValues(){
+    this.accountService.getUserAddress().subscribe(address =>{
+      if(address){
+        this.checkoutForm.get('addressForm').patchValue(address);
+      }
+    }, err =>{
+      console.log(err);
+    })
   }
 
 }
