ea4b040c0a542277e4baa6232ce47004153b2c86
BÙI TẤN LÂN <tanlanhcmus@gmail.com>
2021-02-08 16:29:59 +0700

    update order summary when change delivery

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -40,6 +40,7 @@ export class BasketService {
     return this.http.get(this.baseUrl + 'basket?id=' + id).pipe(
       map((basket: IBasket) => {
         this.basketSource.next(basket);
+        this.shipping = basket.shippingPrice;
         this.calculateTotalPrice();
         this.calculateTotalQuantity();
       })
